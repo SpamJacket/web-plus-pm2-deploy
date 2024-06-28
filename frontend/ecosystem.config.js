d47248568ea316f748a44105e995f499babdaaf5
Aleksandr Svatkov <saealeksandrsae@mail.ru>
2024-06-28 21:58:59 +0300

    Backend deploy config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-unresolved
+require("dotenv").config({ path: "./.env.deploy" });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH } =
+  process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
+    },
+  },
+};
